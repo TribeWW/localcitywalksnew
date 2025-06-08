6968d14b5b9eed7fff75eb3e02dcba8df8c2d574
Matthias Plancke <tioplancke@hotmail.com>
2025-06-08 19:54:16 +0200

    Added background gradient for LocalCityWalks

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen text-4xl font-bold">
-      Coming Soon: LocalCityWalks
-    </div>
+    <>
+      <div className="flex justify-center items-center h-screen text-4xl font-bold">
+        <div className="text-center">
+          <div className="mb-8 text-xl">Coming Soon</div>
+          <div className="mb-8">LocalCityWalks</div>
+          <div className="text-2xl ">An amazing way to explore your city</div>
+        </div>
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
+    </>
   );
 }
