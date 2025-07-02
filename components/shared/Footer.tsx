d791722fdcf901a77f219fa81e5c5e04d6c6ffc1
Matthias Plancke <tioplancke@hotmail.com>
2025-07-02 23:20:40 +0200

    Comment out unused Image import in Footer component for cleaner code organization.

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from "next/image";
+//import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
 import Link from "next/link";
 //import { Separator } from "@/components/ui/separator";
