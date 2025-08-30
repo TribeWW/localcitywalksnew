c6621ad629319671884f6ddfc30edcd6ef86b8ef
Matthias Plancke <tioplancke@hotmail.com>
2025-08-30 17:40:22 +0200

    Add environment variable support for Google Tag Manager and Google Analytics in layout. Update scripts to use dynamic IDs for improved tracking configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,10 @@ import MobileHeader from "@/components/shared/MobileHeader";
 import Footer from "@/components/shared/Footer";
 import Script from "next/script";
 
+// Environment variables for analytics
+const GA4_ID = process.env.NEXT_PUBLIC_GA4_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 const outfit = Outfit({
   variable: "--font-outfit",
   subsets: ["latin"],
@@ -73,26 +77,32 @@ export default function RootLayout({
     <html lang="en" className={outfit.className}>
       <head>
         {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {GTM_ID && (
+          <Script id="google-tag-manager" strategy="beforeInteractive">
+            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MH3M476G');`}
-        </Script>
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+          </Script>
+        )}
         {/* Google tag (gtag.js) */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-PQC1GK2TFT"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-PQC1GK2TFT');
-        `}
-        </Script>
+        {GA4_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA4_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA4_ID}');
+            `}
+            </Script>
+          </>
+        )}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -123,14 +133,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         className={`${outfit.className} antialiased h-screen min-h-screen bg-gradient-to-r from-tangerine to-grapefruit`}
       >
         {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MH3M476G"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+        {GTM_ID && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
         {/* End Google Tag Manager (noscript) */}
         <Header />
         <MobileHeader />
